0150a646ad460ab7f3548af3d63c765182109b25
taittgcs200379 <taittgcs200379@fpt.edu.vn>
2023-10-16 13:10:50 +0700

    ok

--- Assets/Script/Box.cs
+++ Assets/Script/Box.cs
@@ -4,22 +4,14 @@ using UnityEngine;
 
 public class Box : MonoBehaviour
 {
-    public GameObject box;
-    public float radius = 1f;
+    
     private void OnCollisionEnter(Collision collision)
     {
 
-        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
-        foreach (Collider collider in colliders)
-        {
-            if (collider.gameObject.tag.Equals("Bullet"))
+            if (collision.gameObject.tag.Equals("Box"))
             {
                 DestroyBox();
             }
-
-            
-
-        }
     }
     private void DestroyBox()
     {
